595526440371be009729759dcf7b2e7985c15472
yeesheng <yeeshengtan1127@gmail.com>
2023-10-14 14:50:24 +0800

    enable swagger

--- SeniorFoodOrderSystem_BackEnd_Order_Stall_Rating/Program.cs
+++ SeniorFoodOrderSystem_BackEnd_Order_Stall_Rating/Program.cs
@@ -61,11 +61,8 @@ builder.Services.AddControllersWithViews()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
